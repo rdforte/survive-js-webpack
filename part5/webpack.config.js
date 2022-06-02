aaf2cb9fc024998252c02a00b0f9493e7f06648d
Ryan Forte <forte.ryan@hotmail.com>
2022-06-03 01:06:32 +1000

    setup webpack to seperate css into seperate file to stop fouc (flash of unstyled content)

---
+++ part5/webpack.config.js
@@ -0,0 +1,30 @@
+const { mode } = require('webpack-nano/argv')
+const { merge } = require('webpack-merge')
+const parts = require('./webpack.parts')
+
+const commonConfig = merge([
+	{ entry: ['./src'] },
+	parts.page({ title: 'Demo' }),
+	// parts.loadCSS(),
+	parts.extractCSS()
+])
+
+const productionConfig = merge([])
+
+const developmentConfig = merge([
+	{ entry: ['webpack-plugin-serve/client'] },
+	parts.devServer()
+])
+
+const getConfig = mode => {
+	switch (mode) {
+		case 'production':
+			return merge(commonConfig, productionConfig, { mode })
+		case 'development':
+			return merge(commonConfig, developmentConfig, { mode })
+		default:
+			throw new Error('trying to use unknown mode: ', mode)
+	}
+}
+
+module.exports = getConfig(mode)
