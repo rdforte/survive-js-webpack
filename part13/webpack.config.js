1804c9f34f4785d681816aa31d45d5a455eebacf
Ryan Forte <forte.ryan@hotmail.com>
2022-06-19 15:06:51 +1000

    code and bundle splitting

---
+++ part13/webpack.config.js
@@ -0,0 +1,45 @@
+const { mode, target, output } = require('webpack-nano/argv')
+const { merge } = require('webpack-merge')
+const path = require('path')
+const parts = require('./webpack.parts')
+
+const cssLoaders = [parts.autoprefixer(), parts.tailwind()]
+
+const commonConfig = merge([
+	{ entry: ['./src'] },
+	parts.page({ title: 'Demo' }),
+	// parts.loadCSS(),
+	parts.extractCSS({ loaders: cssLoaders }),
+	parts.babelLoader(),
+	parts.loadImages({ limit: 1000 }),
+	parts.loadJavascript(),
+	parts.pathAlias(),
+	parts.generateSourceMaps({ type: 'source-map' }),
+	{ mode },
+	{ target },
+	{
+		output: {
+			path: path.resolve(__dirname, output)
+		}
+	}
+])
+
+const productionConfig = merge([parts.eliminateUnusedCSS()])
+
+const developmentConfig = merge([
+	{ entry: ['webpack-plugin-serve/client'] },
+	parts.devServer()
+])
+
+const getConfig = mode => {
+	switch (mode) {
+		case 'production':
+			return merge(commonConfig, productionConfig)
+		case 'development':
+			return merge(commonConfig, developmentConfig)
+		default:
+			throw new Error('trying to use unknown mode: ', mode)
+	}
+}
+
+module.exports = getConfig(mode)
---
+++ part14/src/components/comp.js
@@ -0,0 +1,18 @@
+// import lazy from './lazy' // ! not lazy
+
+export default (text = 'Hello World') => {
+	const element = document.createElement('div')
+
+	element.className = 'rounded bg-red-100 border max-w-md m-4 p-4'
+	element.innerHTML = text
+
+	// element.onclick = () => (element.textContent = lazy) // ! not lazy
+
+	// LAZY Loading module using dyanmic import
+	element.onclick = () =>
+		import('./lazy')
+			.then(lazy => (element.textContent = lazy.default))
+			.catch(err => console.log(err))
+
+	return element
+}
